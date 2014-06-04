142d80e0fba48a6dac399cab161be96492c26061
Alex Jose <alexnj@gmail.com>
2014-06-03 21:58:12 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Alex on 6/3/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
