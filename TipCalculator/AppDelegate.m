b828540a1e3705bd7cb53aa70b93f22b850f4998
Alex Jose <alexnj@gmail.com>
2014-06-03 22:57:30 -0700

    Tip calculator controls in place.

--- TipCalculator/AppDelegate.m
+++ TipCalculator/AppDelegate.m
@@ -7,12 +7,17 @@
 //
 
 #import "AppDelegate.h"
+#import "TipViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    TipViewController *tvc = [[TipViewController alloc] init];
+    self.window.rootViewController = tvc;
+    
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
---
+++ TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  TipCalculator
+//
+//  Created by Alex on 6/3/14.
+//  Copyright (c) 2014 alexnj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
